56493ad33fb2b2a2f83383e3ac0ed06be59b7671
Ehsan <144959137+TssHack@users.noreply.github.com>
2025-10-08 03:15:13 +0330

    Update CustomIcons.kt

--- app/src/main/java/com/fazli/vispar/ui/theme/CustomIcons.kt
+++ app/src/main/java/com/fazli/vispar/ui/theme/CustomIcons.kt
@@ -6,13 +6,11 @@ import androidx.compose.ui.res.vectorResource
 import com.fazli.vispar.R
 
 object CustomIcons {
-    // فقط شناسه‌های منابع را نگه می‌داریم
     val Movie: Int = R.drawable.ic_movie
     val Series: Int = R.drawable.ic_series
     val Search: Int = R.drawable.ic_search
     val Settings: Int = R.drawable.ic_settings
     
-    // تابع کمکی برای تبدیل شناسه به ImageVector
     @Composable
     fun toImageVector(resourceId: Int): ImageVector {
         return vectorResource(id = resourceId)
