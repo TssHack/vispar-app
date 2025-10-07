f52961c8323114bf03cc7ef009307fd9764fc7f8
Ehsan <144959137+TssHack@users.noreply.github.com>
2025-10-07 19:25:24 +0330

    Update DownloadOptionsDialog.kt

--- app/src/main/java/com/fazli/vispar/components/DownloadOptionsDialog.kt
+++ app/src/main/java/com/fazli/vispar/components/DownloadOptionsDialog.kt
@@ -1,5 +1,5 @@
 package com.fazli.vispar.components
-
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
