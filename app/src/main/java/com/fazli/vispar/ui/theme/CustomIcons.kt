9f8d12e4100027073551b99063605981f2eb2c29
Ehsan <144959137+TssHack@users.noreply.github.com>
2025-10-08 03:02:33 +0330

    Update CustomIcons.kt

--- app/src/main/java/com/fazli/vispar/ui/theme/CustomIcons.kt
+++ app/src/main/java/com/fazli/vispar/ui/theme/CustomIcons.kt
@@ -1,12 +1,17 @@
 package com.fazli.vispar.ui.theme
 
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
-import com.fazli.vispar.R // این خط را اضافه کنید
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import com.fazli.vispar.R
 
 object CustomIcons {
-    val Movie: ImageVector = vectorResource(R.drawable.ic_movie)
-    val Series: ImageVector = vectorResource(R.drawable.ic_series)
-    val Search: ImageVector = vectorResource(R.drawable.ic_search)
-    val Settings: ImageVector = vectorResource(R.drawable.ic_settings)
+    @Composable
+    fun Movie(): Painter = painterResource(R.drawable.ic_movie)
+    @Composable
+    fun Series(): Painter = painterResource(R.drawable.ic_series)
+    @Composable
+    fun Search(): Painter = painterResource(R.drawable.ic_search)
+    @Composable
+    fun Settings(): Painter = painterResource(R.drawable.ic_settings)
 }
